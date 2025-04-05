07178e6a9b3b9c38489db2718a05829eab8ce925
harris-miller <harrismillerconsulting@gmail.com>
2025-04-04 21:02:52 -0600

    getting tables setup

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from 'drizzle-orm';
 import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 import { ulid } from 'ulid';
 
@@ -10,3 +11,53 @@ export const usersTable = pgTable('users', {
     .primaryKey(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
+
+export const charactersTable = pgTable('characters', {
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  id: uuid('id')
+    .$defaultFn(() => ulid())
+    .primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  characters: many(charactersTable),
+}));
+
+export const campaignsTable = pgTable('campaigns', {
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  id: uuid('id')
+    .$defaultFn(() => ulid())
+    .primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const encountersTable = pgTable('encounters', {
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  id: uuid('id')
+    .$defaultFn(() => ulid())
+    .primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const campaignsRelations = relations(campaignsTable, ({ one, many }) => ({
+  characters: many(charactersTable),
+  encounters: many(encountersTable),
+  owner: one(usersTable),
+}));
+
+export const monstersTable = pgTable('encounters', {
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  id: uuid('id')
+    .$defaultFn(() => ulid())
+    .primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const encountersRelations = relations(encountersTable, ({ many }) => ({
+  monsters: many(monstersTable),
+}));
