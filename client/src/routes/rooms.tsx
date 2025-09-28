ccdb909c0071e6bfe85eee16272a871c9381255d
harris-miller <harrismillerconsulting@gmail.com>
2025-09-28 12:48:16 -0600

    there we go

--- client/src/routes/rooms.tsx
+++ client/src/routes/rooms.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 import type { FC } from 'react';
 
@@ -5,10 +6,10 @@ import { Sidebar } from '../components/Sidebar';
 
 export const RoomsLayout: FC = () => {
   return (
-    <>
+    <Box display="flex">
       <Sidebar />
       <Outlet />
-    </>
+    </Box>
   );
 };
 
