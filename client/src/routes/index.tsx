93e3d09a03351ac16cf4b3c906d1f78751c5fd5c
harris-miller <harrismillerconsulting@gmail.com>
2026-02-25 03:00:50 -0700

    new auth user-journey

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,17 +1,14 @@
 import ChatIcon from '@mui/icons-material/Chat';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
-import { Alert, Box, Button, Container, Paper, TextField, Typography } from '@mui/material';
+import { Box, Button, Container, Paper, Typography } from '@mui/material';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
-import type { AxiosError } from 'axios';
-import { useState } from 'react';
 import type { FC } from 'react';
 
-import type { ApiError } from '../api/api.types';
-import { getProfile, signIn } from '../api/user';
 import { FullScreenCenter } from '../components/FullScreenCenter';
 import { useStore } from '../store';
-import { handle } from '../utils';
+
+import { SignUp } from './-signup';
 
 // import { SignIn } from './-signin';
 // import { SignUp } from './-signup';
@@ -19,20 +16,6 @@ import { handle } from '../utils';
 const IndexComponent: FC = () => {
   const navigate = useNavigate();
   const user = useStore(state => state.user);
-  const { setUser } = useStore();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
-  const signInHandler = async () => {
-    try {
-      const response = await signIn(email, password).then(getProfile);
-      setUser(response.data);
-    } catch (err) {
-      setErrorMessage((err as AxiosError<ApiError>).response?.data.message ?? 'Unable to reach server');
-    }
-  };
 
   // TODO: this nav should happen not in the index page, but in the pre-loader
   if (user != null) {
@@ -67,26 +50,7 @@ const IndexComponent: FC = () => {
           <Box sx={{ marginBottom: '12px' }}>
             <Typography>Or</Typography>
           </Box>
-          <Box sx={{ marginBottom: '12px' }}>
-            <Box>
-              <TextField id="email" label="Email" onChange={handle(setEmail)} value={email} />
-            </Box>
-            <Box>
-              <TextField
-                id="password"
-                label="Password"
-                onChange={handle(setPassword)}
-                type="password"
-                value={password}
-              />
-            </Box>
-          </Box>
-          <Box sx={{ marginBottom: '12px' }}>
-            <Button onClick={signInHandler} sx={{ marginBottom: '12px', width: '100%' }} variant="contained">
-              Sign In with Email
-            </Button>
-            {errorMessage != null ? <Alert severity="error">{errorMessage}</Alert> : null}
-          </Box>
+          <SignUp />
         </Paper>
       </Container>
     </FullScreenCenter>
