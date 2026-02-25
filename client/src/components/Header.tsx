e3a8b7513f6fe27a2da9b589e1c1b4d8df91d0a0
harris-miller <harrismillerconsulting@gmail.com>
2026-02-25 03:07:47 -0700

    new <Header>

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,129 @@
+import ChatIcon from '@mui/icons-material/Chat';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import { AppBar, ButtonGroup, IconButton, Popover, Toolbar, Typography } from '@mui/material';
+import { useColorScheme } from '@mui/material/styles';
+import { useState } from 'react';
+import type { FC, MouseEvent } from 'react';
+import { match } from 'ts-pattern';
+
+export const Header: FC = () => {
+  const { mode, setMode } = useColorScheme();
+
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography component="h1" sx={{ flexGrow: 1 }} variant="h6">
+            <ChatIcon /> Scaled Chat App
+          </Typography>
+          <IconButton color="inherit" onClick={handleClick}>
+            {match(mode!)
+              .with('light', () => <LightModeIcon />)
+              .with('system', () => <SettingsBrightnessIcon />)
+              .with('dark', () => <DarkModeIcon />)
+              .exhaustive()}
+          </IconButton>
+          <Popover
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              horizontal: 'center',
+              vertical: 'bottom',
+            }}
+            onClose={handleClose}
+            open={open}
+            transformOrigin={{
+              horizontal: 'center',
+              vertical: 'top',
+            }}
+          >
+            <ButtonGroup aria-label="Basic button group" variant="contained">
+              <IconButton
+                onClick={() => {
+                  setMode('light');
+                }}
+              >
+                <LightModeIcon />
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  setMode('system');
+                }}
+              >
+                <SettingsBrightnessIcon />
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  setMode('dark');
+                }}
+              >
+                <DarkModeIcon />
+              </IconButton>
+            </ButtonGroup>
+          </Popover>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
+};
+
+// export const Header: FC = () => {
+//   return (
+//     <AppBar position="fixed" sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}>
+//       <Toolbar>
+//         <IconButton
+//           aria-label="menu"
+//           color="inherit"
+//           edge="start"
+//           onClick={() => navigate({ to: '/' })}
+//           size="large"
+//           sx={{ mr: 2 }}
+//         >
+//           <MenuIcon />
+//         </IconButton>
+//         <Typography component="div" sx={{ flexGrow: 1 }} variant="h1">
+//           Scaled Chat App
+//         </Typography>
+//         {user != null && (
+//           <>
+//             <IconButton aria-label="menu" color="inherit" edge="start" onClick={handleMenu} size="large" sx={{ mr: 2 }}>
+//               {user.profilePicId != null ? (
+//                 <Avatar alt="Profile" src={`/api/user/profile/pic/${user.profilePicId}/thumb`} />
+//               ) : (
+//                 <PersonIcon />
+//               )}
+//             </IconButton>
+//             <Menu
+//               anchorEl={anchorEl}
+//               anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+//               id="menu-appbar"
+//               onClose={handleClose}
+//               open={Boolean(anchorEl)}
+//             >
+//               <ListSubheader>
+//                 {user.displayName} ({user.email})
+//               </ListSubheader>
+//               <Divider />
+//               <MenuItem onClick={profileHandler}>Profile</MenuItem>
+//               <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+//             </Menu>
+//           </>
+//         )}
+//       </Toolbar>
+//     </AppBar>
+//   );
+// };
--- client/src/routes/rooms/route.tsx
+++ client/src/routes/rooms/route.tsx
@@ -1,19 +1,16 @@
-import { Flex } from '@radix-ui/themes';
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 import type { FC } from 'react';
 
-import { Sidebar } from '../../components/Sidebar';
-import { TabRail } from '../../components/TabRail';
-import { Toolbar } from '../../components/Toolbar';
+import { Header } from '../../components/Header';
 
 import './route.css';
 
 export const RoomsLayout: FC = () => {
   return (
     <>
-      <div className="background" />
-      <Toolbar />
-      <Flex className="rooms-container">
+      {/* <div className="background" /> */}
+      <Header />
+      {/* <Flex className="rooms-container">
         <Flex width="440px">
           <TabRail />
           <Sidebar />
@@ -21,7 +18,7 @@ export const RoomsLayout: FC = () => {
         <Flex className="main" flexGrow="1" p="3">
           <Outlet />
         </Flex>
-      </Flex>
+      </Flex> */}
     </>
   );
 };
