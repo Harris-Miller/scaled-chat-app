6fb8889fb800343714bde61935aee018f1d23af8
harris-miller <harrismillerconsulting@gmail.com>
2026-02-25 01:35:02 -0700

    coming soon

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,12 +1,17 @@
 import ChatIcon from '@mui/icons-material/Chat';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
-import { Box, Button, Container, Paper, TextField, Typography } from '@mui/material';
+import { Alert, Box, Button, Container, Paper, TextField, Typography } from '@mui/material';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import type { AxiosError } from 'axios';
+import { useState } from 'react';
 import type { FC } from 'react';
 
+import type { ApiError } from '../api/api.types';
+import { getProfile, signIn } from '../api/user';
 import { FullScreenCenter } from '../components/FullScreenCenter';
 import { useStore } from '../store';
+import { handle } from '../utils';
 
 // import { SignIn } from './-signin';
 // import { SignUp } from './-signup';
@@ -14,6 +19,21 @@ import { useStore } from '../store';
 const IndexComponent: FC = () => {
   const navigate = useNavigate();
   const user = useStore(state => state.user);
+  const { setUser } = useStore();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  // TODO
+  const _signInHandler = async () => {
+    try {
+      const response = await signIn(email, password).then(getProfile);
+      setUser(response.data);
+    } catch (err) {
+      setErrorMessage((err as AxiosError<ApiError>).response?.data.message ?? 'Unable to reach server');
+    }
+  };
 
   // TODO: this nav should happen not in the index page, but in the pre-loader
   if (user != null) {
@@ -31,10 +51,18 @@ const IndexComponent: FC = () => {
           <Typography sx={{ marginBottom: '24px' }} variant="h5">
             Sign In
           </Typography>
-          <Button startIcon={<GoogleIcon />} sx={{ marginBottom: '12px', width: '100%' }} variant="outlined">
+          <Button
+            startIcon={<GoogleIcon />}
+            sx={{ '&:hover': { cursor: 'not-allowed' }, marginBottom: '12px', width: '100%' }}
+            variant="outlined"
+          >
             Google
           </Button>
-          <Button startIcon={<FacebookIcon />} sx={{ marginBottom: '12px', width: '100%' }} variant="outlined">
+          <Button
+            startIcon={<FacebookIcon />}
+            sx={{ '&:hover': { cursor: 'not-allowed' }, marginBottom: '12px', width: '100%' }}
+            variant="outlined"
+          >
             Facebook
           </Button>
           <Box sx={{ marginBottom: '12px' }}>
@@ -42,16 +70,23 @@ const IndexComponent: FC = () => {
           </Box>
           <Box sx={{ marginBottom: '12px' }}>
             <Box>
-              <TextField id="email" label="Email" />
+              <TextField id="email" label="Email" onChange={handle(setEmail)} value={email} />
             </Box>
             <Box>
-              <TextField id="password" label="Password" type="password" />
+              <TextField
+                id="password"
+                label="Password"
+                onChange={handle(setPassword)}
+                type="password"
+                value={password}
+              />
             </Box>
           </Box>
           <Box sx={{ marginBottom: '12px' }}>
             <Button sx={{ marginBottom: '12px', width: '100%' }} variant="contained">
               Sign In with Email
             </Button>
+            {errorMessage != null ? <Alert severity="error">{errorMessage}</Alert> : null}
           </Box>
         </Paper>
       </Container>
